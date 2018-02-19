4b22c57da969f5f95a72509b3c971601725d35a5
J. Eduardo V. Morango <jevmor@gmail.com>
2018-02-19 20:02:52 +0000

    variance part 1 fixes

--- src/main/scala/variance/VarianceMain.scala
+++ src/main/scala/variance/VarianceMain.scala
@@ -16,23 +16,15 @@ object VarianceMain extends App {
   val grape = new Grape
   val fruit = new Fruit
 
-  //get a child and return the father
+  //In covariance, the focus are into specialized types
   val cov: Covariant[Fruit] = new Covariant[Grape] {
-    override def message(): Fruit = {
+    override def message(): Grape = {
       println("This call is covariant, accepts a child impl")
-      new Fruit
-    }
-  }
-
-  // gets any family member and can specialize it
-  val cov2: Covariant[Fruit] = new Covariant[Fruit] {
-    override def message(): Fruit = {
-      println("This call is covariant who receives any fruit, return any child")
       new Grape
     }
   }
 
-  // Gets the father and can specialize him.
+  // Gets the father and acts like him.
   val contra: Contravariant[Grape] = new Contravariant[Fruit] {
     override def message(a: Fruit): Unit = {
       println(
@@ -41,8 +33,6 @@ object VarianceMain extends App {
   }
 
   cov.message()
-  cov2.message()
-
   contra.message(new Grape)
 
 }
