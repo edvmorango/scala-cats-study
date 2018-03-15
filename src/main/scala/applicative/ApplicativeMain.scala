55d915ab7f3378076565e824d7f5a7fe4797f034
J. Eduardo V. Morango <jevmor@gmail.com>
2018-03-15 16:34:58 +0000

    <*>

--- src/main/scala/applicative/ApplicativeMain.scala
+++ src/main/scala/applicative/ApplicativeMain.scala
@@ -32,4 +32,21 @@ object SemigroupalMain extends App {
 
 }
 
-object ApplicativeMain extends App {}
+object ApplicativeMain extends App {
+  import cats.syntax.apply._
+
+  case class Variadic(a: Int, b: String, c: Double, d: Char)
+
+  // Tupled is variadic
+  println((Option(1), Option("hey")).tupled)
+
+  // Apply arguments into a wrapped structure/function A   --> F[A]   same Haskell <*>
+  val variadicApply =
+    (Option(1), Option("two"), Option(3.0), Option('4')).mapN(Variadic.apply)
+
+  println(s"Variadic Apply: $variadicApply")
+
+  val functionApply = (Option(2), Option(2)).mapN((a, b) => a * b)
+
+  println(s"Function Apply: $functionApply")
+}
